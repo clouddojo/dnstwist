7dce7b68e3f52a2ef177854f6d2547bfde1a242f
John Ayers <jayers99@gmail.com>
2020-09-13 16:21:17 -0700

    add pipenv, remove geoip

--- snippets.sh
+++ snippets.sh
@@ -1,1 +1,11 @@
+# run in docker
 time docker build -t dnstwist . && time docker run dnstwist -gmab -f csv short.com
+
+# g = geoip
+# m = mx record
+# a = all
+# b = site banner
+# run in local env
+./dnstwist.py short.com
+./dnstwist.py -mab short.com
+./dnstwist.py -mab -f json short.com | tee results.json | jq '.'
